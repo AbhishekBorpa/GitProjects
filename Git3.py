74b59d488f1b573a4e10edd1eba2db64cd63b2d3
abhishek <abhishekborpa@gmail.com>
2025-04-25 23:10:43 +0530

    New Python File

---
+++ Git3.py
@@ -0,0 +1,12 @@
+# Simple if-else program in Python
+
+# Input from user
+num = 12
+
+# Check if positive, negative, or zero
+if num > 0:
+    print("The number is positive.")
+elif num < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
